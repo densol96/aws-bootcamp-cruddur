c419095a6fae3b6d3836e55633c02591dc258fd5
densol96 <deniss11sol@gmail.com>
2024-10-21 17:29:54 +0000

    Notifications on front end added

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import HomeFeedPage from './pages/HomeFeedPage';
+import NotificationsFeedPage from './pages/NotificationsFeedPage';
 import UserFeedPage from './pages/UserFeedPage';
 import SignupPage from './pages/SignupPage';
 import SigninPage from './pages/SigninPage';
@@ -47,6 +48,10 @@ const router = createBrowserRouter([
   {
     path: "/forgot",
     element: <RecoverPage />
+  }, 
+  {
+    path: "/notifications",
+    element: <NotificationsFeedPage />
   }
 ]);
 
