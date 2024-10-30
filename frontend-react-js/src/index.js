01cf4f251c072beaaee19ae9d1b8357dcdc7fb15
densol96 <deniss11sol@gmail.com>
2024-10-30 23:20:39 +0000

    Added custom tracer's span to HomeActivities and observed it in honeycomb

--- frontend-react-js/src/index.js
+++ frontend-react-js/src/index.js
@@ -16,3 +16,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+console.log("TEST IS --> ", process.env.TEST);
+console.log(process.env);
