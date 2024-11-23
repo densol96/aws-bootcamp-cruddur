ec837de4c64d54437b15027acf8dca5051e5339b
densol96 <deniss11sol@gmail.com>
2024-11-23 23:01:17 +0000

    docker-compose updated + schema loaded into RDS

--- backend-flask/bin/db-schema-load.sh
+++ backend-flask/bin/db-schema-load.sh
@@ -1,4 +1,9 @@
 #! /usr/bin/bash
 
 schema_path="/workspace/aws-bootcamp-cruddur/backend-flask/db/schema.sql"
-psql $CRUDDUR_DB_CONNECTION_URL < $schema_path
+
+if [ "$1" = "dev" ]; then
+ psql $CRUDDUR_DB_CONNECTION_URL < $schema_path
+else
+ psql $AWS_RDS_CONNECTION_URL < $schema_path   
+fi
