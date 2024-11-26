992bcb124309296f23140f2e427be2e339847b67
densol96 <deniss11sol@gmail.com>
2024-11-26 14:04:35 +0000

    bug with post-confirm lambda fixed and tested

--- aws/lambda/lambda_function.py
+++ aws/lambda/lambda_function.py
@@ -4,25 +4,26 @@ import os
 
 def lambda_handler(event, context):
     user = event['request']['userAttributes']
-    print(user) # {'sub': '20fc191c-20c...', 'email_verified': 'true', 'cognito:user_status': 'CONFIRMED', 'nickname': 'solodeni', 'name': 'Deniss', 'email': 'deniss11sol@gmail.com'}
-    # try:
-    #     conn = psycopg2.connect(
-    #         host=(os.getenv('PG_HOSTNAME')),
-    #         database=(os.getenv('PG_DATABASE')),
-    #         user=(os.getenv('PG_USERNAME')),
-    #         password=(os.getenv('PG_SECRET'))
-    #     )
-    #     cur = conn.cursor()
-    #     cur.execute("INSERT INTO users (display_name, handle, cognito_user_id) VALUES(%s, %s, %s)", (user['name'], user['email'], user['sub']))
-    #     conn.commit() 
+    conn = None
 
-    # except (Exception, psycopg2.DatabaseError) as error:
-    #     print(error)
+    try:
+        conn = psycopg2.connect(
+            os.getenv("PROD_CONNECTION_URL")
+        )
+        cur = conn.cursor()
+        cur.execute(
+            "INSERT INTO users (name, nickname, email, cognito_user_id) VALUES(%s, %s, %s, %s)",
+            (user['name'], user['nickname'], user['email'], user['sub'])
+        )
+        conn.commit() 
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Handled error => ", error)
         
-    # finally:
-    #     if conn is not None:
-    #         cur.close()
-    #         conn.close()
-    #         print('Database connection closed.')
+    finally:
+        if conn is not None:
+            cur.close()
+            conn.close()
+            print('Database connection closed.')
 
     return event
--- backend-flask/bin/db-connect.sh
+++ backend-flask/bin/db-connect.sh
@@ -1,25 +1,10 @@
 #! /usr/bin/bash
-## Example: ENV_VAR = prod
-if [ -z "$ENV_VAR" ]; then
-  echo "Error: ENV_VAR is not set. Please set it to 'dev' or 'prod'."
-  exit 1
+
+if [ "$1" = "dev" ]; then
+ CONNECTION_URL="$CRUDDUR_DB_CONNECTION_URL"
+else
+ CONNECTION_URL="$AWS_RDS_CONNECTION_URL"
 fi
-
-
-case "$ENV_VAR" in
-  dev)
-    echo "Connecting to docker psql."
-    CONNECTION_URL="$CRUDDUR_DB_CONNECTION_URL"
-    ;;
-  prod)
-    echo "Connecting to AWS RDS."
-    CONNECTION_URL="$AWS_RDS_CONNECTION_URL"
-    ;;
-  *)
-    echo "Error: Unrecognized ENV_VAR value '$ENV_VAR'. Use 'dev', 'staging', or 'prod'."
-    exit 1
-    ;;
-esac
 
 psql $CONNECTION_URL
 
